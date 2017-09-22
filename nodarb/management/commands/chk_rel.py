43d1b64e99cbc316644d7279936e43df3734956b
svabis <fizmats@inbox.lv>
2017-09-22 17:09:45 +0300

    relacijas automated at 04:00

--- nodarb/management/commands/chk_rel.py
+++ nodarb/management/commands/chk_rel.py
@@ -15,10 +15,10 @@ class Command(BaseCommand):
         start = datetime.datetime(today.year, today.month, today.day)
         end = today.replace(hour=23, minute=59, second=59) + datetime.timedelta(days=28)
 
-       # Delete all relations
-        Tren_nodarb.objects.all().delete()
+       # All relations
+        rel_exist = Tren_nodarb.objects.all()
 
-       # get grafiks entries for 4 weeks
+       # Get grafiks entries for 4 weeks
         graf = Grafiks.objects.filter(sakums__range=( start, end))
         temp_rel = []
         for g in graf:
@@ -26,20 +26,32 @@ class Command(BaseCommand):
             n = g.nodarbiba
             temp_rel.append([n,t])
 
-        rel_fakt = set(tuple(element) for element in temp_rel)
-        print graf.count() # total count
-        print len(rel_fakt) # unique count
-        print
+        rel_fakt = set(tuple(element) for element in temp_rel) # extract unique from all
+        print graf.count() # grafiks count
+        print len(rel_fakt) # relation count
+
+       # Create relations if not exist
+        print 'creating relations...'
+        temp_rel = []
         for r in list(rel_fakt):
-            new_rel = Tren_nodarb( nodarb=r[0], treneris=r[1])
-            new_rel.save()
-            print r
+            try:
+                obj = Tren_nodarb.objects.get( nodarb=r[0], treneris=r[1] )
+                temp_rel.append(obj) # add to list
+            except:
+                new_rel = Tren_nodarb( nodarb=r[0], treneris=r[1])
+                new_rel.save()
+                temp_rel.append(new_rel) # add to list
+                print r
+        print '...done\n'
 
-#        print
-       # Relacijas Objects all
-#        rel_obj = []
+       # Remove not needed relations
+        print 'deleting unused relations...'
+        temp_rel2 = []
+        for r in rel_exist:
+            temp_rel2.append(r)
 
-#        for t in Tren_nodarb.objects.all():
-#            rel_obj.append(t)
-#        for r in list(set(rel_obj)):
-#            print r
+        unused = list(set(temp_rel2) - set(temp_rel))
+        for u in unused:
+            u.delete()
+            print u
+        print '...done\n'
